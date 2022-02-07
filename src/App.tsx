6bf1ec0fd5050014578aebf8ee7685789010715e
anhmuoi <anhmuoi200147@gmail.com>
2022-02-07 23:14:52 +0700

    first commit side-menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.scss';
+import SideMenu from './components/SideMenu/SideMenu';
 
 function App() {
   return (
     <div className="App">
-      <h1>cc</h1>
+      <SideMenu />
     </div>
   );
 }
---
+++ src/components/SideMenu/SideMenu.tsx
@@ -0,0 +1,160 @@
+import React, { useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import { NavLink, Route, Routes } from 'react-router-dom';
+import arrow from '../../Assets/Images/arrow-icon.png';
+import i18n from '../../i18n';
+import Brochure from '../Pages/Brochure/Brochure';
+import Contact from '../Pages/Contact/Contact';
+import Home from '../Pages/HomePage/Home';
+import Proposal from '../Pages/Proposal/Proposal';
+import './SideMenu.scss';
+
+const SideMenu = () => {
+    const [selected, setSelected] = useState<'ko' | 'en'>('ko');
+    const [showItemBrochure, setShowItemBrochure] = useState<boolean>(false);
+    const [showItemProposal, setShowItemProposal] = useState<boolean>(false);
+    const { t } = useTranslation();
+
+    const [closeSideMenu, setCloseSideMenu] = useState<boolean>();
+
+    const handleChangeLanguage = (lang: 'ko' | 'en') => {
+        if (lang === 'ko') {
+            i18n.changeLanguage('ko');
+            setSelected(lang);
+        } else {
+            i18n.changeLanguage(lang);
+            setSelected(lang);
+        }
+    };
+
+    const handleClickBrochure = () => {
+        setShowItemProposal(false);
+        setShowItemBrochure(!showItemBrochure);
+    };
+    const handleClickProposal = () => {
+        setShowItemBrochure(false);
+        setShowItemProposal(!showItemProposal);
+    };
+    const handleClickHome = () => {
+        setCloseSideMenu(false);
+        setShowItemProposal(false);
+        setShowItemBrochure(false);
+    }
+
+    return (
+        <>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="brochure" element={<Brochure />} />
+                <Route path="proposal" element={<Proposal />} />
+                <Route path="contact" element={<Contact />} />
+            </Routes>
+            <div className="side-menu-container">
+                <nav role="navigation">
+                    <div id="side-menu-toggle">
+                        <input type="checkbox" checked={closeSideMenu} onClick={() => setCloseSideMenu((prev) => !prev)} />
+                        <div className="side-menu-container-change-lang-wrapper">
+                            <div className={`change-lang en ${selected === 'en' ? 'select' : ''}`} onClick={() => handleChangeLanguage('en')}>
+                                EN
+                            </div>
+                            <div className={`change-lang ko ${selected === 'ko' ? 'select' : ''}`} onClick={() => handleChangeLanguage('ko')}>
+                                KR
+                            </div>
+                        </div>
+
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                        <ul id="menu">
+                            <div className="side-menu-container-change-lang-menu">
+                                <div
+                                    className={`side-menu-container-change-lang-menu-item ko ${selected === 'en' ? 'select' : ''}`}
+                                    onClick={() => handleChangeLanguage('en')}
+                                >
+                                    EN
+                                </div>
+                                <div
+                                    className={`side-menu-container-change-lang-menu-item ko ${selected === 'ko' ? 'select' : ''}`}
+                                    onClick={() => handleChangeLanguage('ko')}
+                                >
+                                    KR
+                                </div>
+                            </div>
+                            <NavLink className="list" to="/" onClick={() => handleClickHome()}>
+                                <li>Home</li>
+                            </NavLink>
+                            <NavLink className="list" to="/brochure" onClick={() => handleClickBrochure()}>
+                                <li className={`${showItemBrochure ? 'choose' : ''}`}>Brochure</li>
+                                <ul className={`brochure-list ${showItemBrochure ? '' : 'disable'}`}>
+                                    <li className="brochure-item">
+                                        <p>Download-1</p> <img src={arrow} alt=""></img>
+                                    </li>
+                                    <li className="brochure-item">
+                                        <p>Download-2</p> <img src={arrow} alt=""></img>
+                                    </li>
+                                    <li className="brochure-item">
+                                        <p>Download-3</p> <img src={arrow} alt=""></img>
+                                    </li>
+                                    <li className="brochure-item">
+                                        <p>Download-4</p> <img src={arrow} alt=""></img>
+                                    </li>
+                                    <li className="brochure-item">
+                                        <p>Download-5</p> <img src={arrow} alt=""></img>
+                                    </li>
+                                </ul>
+                            </NavLink>
+                            <NavLink className="list" to="/proposal" onClick={() => handleClickProposal()}>
+                                <li className={`${showItemProposal ? 'choose' : ''}`}>Proposal</li>
+                                <ul className={`proposal-list ${showItemProposal ? '' : 'disable'}`}>
+                                    <li className="proposal-item">
+                                        <p>Download-1</p> <img src={arrow} alt=""></img>
+                                    </li>
+                                    <li className="proposal-item">
+                                        <p>Download-2</p> <img src={arrow} alt=""></img>
+                                    </li>
+                                    <li className="proposal-item">
+                                        <p>Download-3</p> <img src={arrow} alt=""></img>
+                                    </li>
+                                </ul>
+                            </NavLink>
+                            <NavLink className="list" to="/contact" onClick={() => handleClickHome()}>
+                                <li>Contact</li>
+                            </NavLink>
+                            <div className="info-wrapper">
+                                <p id="address">
+                                    {t('address')}
+                                    <br />
+                                    Tel 02. 851. 2662/ Fax 02. 851. 2662
+                                </p>
+                                <p className="link">
+                                    <a
+                                        href="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3164.6611743669755!2d127.0209823151663!3d37.51590877980742!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x357ca3e9253866e9%3A0xc7c3669311c4500c!2z7ISc7Jq47Yq567OE7IucIOqwleuCqOq1rCDrhbztmITrj5kg64-E7IKw64yA66GcOOq4uCAxNy0xMCAxRg!5e0!3m2!1sko!2skr!4v1574831755695!5m2!1sko!2skr"
+                                        target="blank"
+                                    >
+                                        View Map
+                                    </a>
+                                </p>
+                                <br />
+                                <p>Company.</p>
+                                <p className="link">
+                                    <a href="mailto:support@taejin.co.kr" title="">
+                                        support@taejin.co.kr
+                                    </a>
+                                </p>
+                                <br />
+                                <p>Technical support</p>
+                                <p className="link">
+                                    <a href="mailto:support@taejin.co.kr" title="">
+                                        support@taejin.co.kr
+                                    </a>
+                                </p>
+                            </div>
+                        </ul>
+                    </div>
+                </nav>
+            </div>
+        </>
+    );
+};
+
+export default SideMenu;
