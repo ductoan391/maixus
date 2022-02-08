99845980086e8a5ef31f5b84f83de89b0d310267
Duc Toan <ductoan391@gmail.com>
2022-02-08 14:13:26 +0700

    contact

---
+++ src/components/Pages/Contact/MapPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function MapPage() {
+    return <div>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3164.6611743669755!2d127.0209823151663!3d37.51590877980742!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x357ca3e9253866e9%3A0xc7c3669311c4500c!2z7ISc7Jq47Yq567OE7IucIOqwleuCqOq1rCDrhbztmITrj5kg64-E7IKw64yA66GcOOq4uCAxNy0xMCAxRg!5e0!3m2!1sko!2skr!4v1574831755695!5m2!1sko!2skr" style={{
+            border: 0
+        }} width="100%" height="770px" loading="lazy" title="Maps"></iframe>
+    </div>;
+}
+
+export default MapPage;
