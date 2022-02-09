d1f2c38af1ed2a819dd5a8447ff4a6fdcaedca9a
anhmuoi <anhmuoi200147@gmail.com>
2022-02-09 13:53:36 +0700

    optimal code

--- src/components/Pages/HomePage/Home.tsx
+++ src/components/Pages/HomePage/Home.tsx
@@ -1,70 +1,52 @@
 import React from 'react';
-import { useEffect, useState } from "react";
-import { gsap } from "gsap";
-import { useNavigate } from "react-router-dom";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
-import { ScrollToPlugin } from "gsap/all";
+import { useEffect, useState } from 'react';
+import { gsap } from 'gsap';
+import { useNavigate } from 'react-router-dom';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import { ScrollToPlugin } from 'gsap/all';
 import FirstPage from './FirstPage/FirstPage';
 import SecondPage from './SecondPage/SecondPage';
 import ThirdPage from './ThirdPage/ThirdPage';
 import FourthPage from './FourthPage/FourthPage';
 import FiifthPage from './FifthPage/FiifthPage';
-import "fullpage.js/vendors/scrolloverflow"; // Optional. When using scrollOverflow:true
-import ReactFullpage from "@fullpage/react-fullpage";
-
+import 'fullpage.js/vendors/scrolloverflow'; // Optional. When using scrollOverflow:true
+import ReactFullpage from '@fullpage/react-fullpage';
 
 gsap.registerPlugin(ScrollTrigger);
 gsap.registerPlugin(ScrollToPlugin);
 const Home = () => {
-  
-
-
-
-  const anchors = ["#firstPage", "#secondPage", "#thirdPage", "#fourthPage", "#fifthPage"];
+    const anchors = ['#firstPage', '#secondPage', '#thirdPage', '#fourthPage', '#fifthPage'];
 
     return (
-      <ReactFullpage
-      anchors={anchors}
-      navigation
-      navigationTooltips={anchors}
-      sectionsColor={["#282c34", "#ff5f45", "#0798ec"]}
-      scrollingSpeed={700}
-      onLeave={(origin, destination, direction) => {
-
-     
-        console.log("onLeave event", { origin, destination, direction });
-      }}
-      render={({ state, fullpageApi }) => {
-        
-        console.log("render prop change", state); // eslint-disable-line no-console
-    
-     
-  
-        return (
-          <>
-          <div className='section'>
-          <FirstPage ></FirstPage>
-          </div>
-          <div className='section'>
-          <SecondPage ></SecondPage>
-          </div>
-          <div className='section'>
-          <ThirdPage ></ThirdPage>
-          </div>
-          <div className='section'>
-          <FourthPage ></FourthPage>
-          </div>
-          <div className='section'>
-          <FiifthPage ></FiifthPage>
-          </div>
-          </>
-        );
-      }}
-    />
+        <ReactFullpage
+            anchors={anchors}
+            navigation
+            navigationTooltips={anchors}
+            scrollingSpeed={700}
+            onLeave={(origin, destination, direction) => {}}
+            render={() => {
+                return (
+                    <>
+                        <div className="section">
+                            <FirstPage></FirstPage>
+                        </div>
+                        <div className="section">
+                            <SecondPage></SecondPage>
+                        </div>
+                        <div className="section">
+                            <ThirdPage></ThirdPage>
+                        </div>
+                        <div className="section">
+                            <FourthPage></FourthPage>
+                        </div>
+                        <div className="section">
+                            <FiifthPage></FiifthPage>
+                        </div>
+                    </>
+                );
+            }}
+        />
     );
 };
 
 export default Home;
-
-
-
